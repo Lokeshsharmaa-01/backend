0f23ab2f89d18c5c6704ece6d405cc70bfb55644
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-03-23 10:30:41 +0530

    add user model

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,32 @@
-import mogoose from  "mogoose";
+// import mogoose from  "mogoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB = async () =>{
+//     try {
+//         const connectionInstance = await mogoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         console.log(`\n MongoDb connected !! Db host: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("Mongodb connection error",error)
+//         process.exit(1);
+//     }
+// }
+
+
+// export default connectDB
+
+
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async () =>{
+
+const connectDB = async () => {
     try {
-        const connectionInstance = await mogoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDb connected !! Db host: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("Mongodb connection error",error)
-        process.exit(1);
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
 }
-
 
 export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
+// import dotenv from "dotenv"
+// import connectDB from "./db/index.js";
+
+
+// // import { Express } from "express";
+// dotenv.config({
+//     path: './env'
+// })
+
+// connectDB()
+// .then(()=>{
+//     app.listen(process.env.PORT || 8000, ()=>{
+//         console.log(`Server is running at port: ${process.env.PORT}`);
+//     })
+// })
+// .catch((err)=>{
+//     console.log("Mongo db connection failde !!",err)
+// })
+
+
+
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
-
-// import { Express } from "express";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at port: ${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)=>{
-    console.log("Mongo db connection failde !!",err)
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
